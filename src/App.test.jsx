c05d03ce811dd891676f840851422baa1068217d
Maxnikit <max.nikit.03@gmail.com>
2024-04-17 16:05:42 +0300

    Add and pass 1 test

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import App from "./App";
+import { render, screen } from "@testing-library/react";
+
+describe("App components", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+  });
+});
