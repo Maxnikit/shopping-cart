9db806f3b2aec46f2047465599eeb85f84383a0f
Maxnikit <max.nikit.03@gmail.com>
2024-04-18 02:34:13 +0300

    Fix ts errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  BrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import routes from "./routes";
