f8802d0e3543614159711e0b57cdebd55b8914df
Maxnikit <max.nikit.03@gmail.com>
2024-04-18 22:11:46 +0300

    Change render to custom render because i need MantineProvider in test

--- src/components/ErrorPage/ErrorPage.test.jsx
+++ src/components/ErrorPage/ErrorPage.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import { describe, it, expect } from "vitest";
 import App from "../../App";
