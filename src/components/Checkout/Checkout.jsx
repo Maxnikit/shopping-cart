55d49c0e4647bfcf6b778091212d21e8bea47fef
Maxnikit <max.nikit.03@gmail.com>
2024-07-03 18:13:22 +0300

    Add Check icon on checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -18,6 +18,7 @@ import {
 import { useCart } from "../../hooks/cartContext";
 import { Link } from "react-router-dom";
 import style from "./Checkout.module.css";
+import { IconCheck } from "@tabler/icons-react";
 const Checkout = () => {
   const {
     cartItems,
@@ -32,6 +33,7 @@ const Checkout = () => {
   return (
     <Card withBorder maw={500} m="auto" align="center">
       <Title>Thanks for your order!</Title>
+      <IconCheck width="100%" height={200} color="#AE3EC9" />
       <Text>
         Your items will come soon. Meanwhile, you can look what else we have{" "}
         <Link to="/">here!</Link>
