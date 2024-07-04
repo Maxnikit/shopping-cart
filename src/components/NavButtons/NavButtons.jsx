bad6eb9d1d89cd84bf3ca8b90c0ef2d808e59995
Maxnikit <max.nikit.03@gmail.com>
2024-07-04 18:52:45 +0300

    Remove finished TODO

--- src/components/NavButtons/NavButtons.jsx
+++ src/components/NavButtons/NavButtons.jsx
@@ -1,8 +1,6 @@
-import { Button, NavLink } from "@mantine/core";
+import { Button } from "@mantine/core";
 import { Link } from "react-router-dom";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { IconHome2, IconShoppingCart } from "@tabler/icons-react";
 
 import classes from "./NavButtons.module.css";
@@ -13,10 +11,6 @@ const links = [
 ];
 
 const NavButtons = (burgerToggle) => {
-  // TODO figure out why using Link leads to 404 when clicking it or manuall typing it in URL bar.
-  // Link is generally better for navBar than navigate because it is semantically correct and allows for
-  // rightclick - open in new tab
-
   const items = links.map((link) => (
     <Button
       component={Link}
@@ -25,7 +19,6 @@ const NavButtons = (burgerToggle) => {
       to={link.link}
       className={classes.link}
       leftSection={link.icon}
-      // onClick={(event) => event.preventDefault()}
     >
       {link.label}
     </Button>
