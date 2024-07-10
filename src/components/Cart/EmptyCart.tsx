4f4691e7f210ef654b4064a69ca332f47e99ac7f
Maxnikit <max.nikit.03@gmail.com>
2024-07-10 22:55:20 +0300

    Add image to EmptyCart

--- src/components/Cart/EmptyCart.tsx
+++ src/components/Cart/EmptyCart.tsx
@@ -1,4 +1,5 @@
 import { Button, Card, Stack, Text, Title } from "@mantine/core";
+import { IconShoppingCartOff } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
@@ -6,6 +7,7 @@ const EmptyCart = () => {
     <Card withBorder maw={500} p="lg" m="auto" ta="center">
       <Stack align="center">
         <Title order={3}>Looks like your cart is empty!</Title>
+        <IconShoppingCartOff width="100%" height={200} color="#AE3EC9" />
         <Text>Why not look at what we have at shop page?</Text>
         <Button size="md" component={Link} to="/shop">
           Shop page
