524d404baf3b56a99768ad6e432d3a3ba2afe4ca
Maxnikit <max.nikit.03@gmail.com>
2024-07-10 21:53:02 +0300

    Remove onBlur submit and fix typescript error

--- src/components/AutocompleteExtended/AutocompleteExtended.tsx
+++ src/components/AutocompleteExtended/AutocompleteExtended.tsx
@@ -8,16 +8,6 @@ const AutocompleteExtended: React.FC<AutocompleteExtendedProps> = ({
   customOnNovelOptionSubmit,
   ...props
 }) => {
-  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-    const newValue: string | null =
-      event.target.value.length === 0 ? null : event.target.value;
-
-    checkOption(newValue);
-
-    // pass to original onBlur event
-    props.onBlur && props.onBlur(event);
-  };
-
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       // Directly assign the value without checking for length
@@ -34,13 +24,11 @@ const AutocompleteExtended: React.FC<AutocompleteExtendedProps> = ({
 
   const checkOption = (newValue: string | null) => {
     if (newValue !== null && !props.data?.includes(newValue)) {
-      props.customOnNovelOptionSubmit?.(newValue);
+      customOnNovelOptionSubmit?.(newValue);
     }
   };
 
-  return (
-    <Autocomplete {...props} onBlur={handleBlur} onKeyDown={handleKeyDown} />
-  );
+  return <Autocomplete {...props} onKeyDown={handleKeyDown} />;
 };
 
 export default AutocompleteExtended;
