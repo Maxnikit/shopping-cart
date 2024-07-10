2e7153343fec90ac0695670b6856ed9dac260536
Maxnikit <max.nikit.03@gmail.com>
2024-07-10 22:08:18 +0300

    Improve NoProductsFound

--- src/components/Shop/NoProductsFound.tsx
+++ src/components/Shop/NoProductsFound.tsx
@@ -1,15 +1,29 @@
-import { Flex, Title, Container, Button } from "@mantine/core";
+import {
+  Flex,
+  Title,
+  Container,
+  Button,
+  Text,
+  Card,
+  Stack,
+} from "@mantine/core";
+import { IconMoodSad } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
 export function NoProductsFound() {
   return (
-    <Flex direction="column" gap={20} align="center">
-      <Title ta="center" order={3}>
-        No products found with current filters
-      </Title>
-      <Button component={Link} to="/shop">
-        Clean filters
-      </Button>
-    </Flex>
+    <Card withBorder maw={500} p="lg" m="auto" ta="center">
+      <Stack align="center">
+        <Title>No Products Found</Title>
+        <IconMoodSad width="100%" height={200} color="#AE3EC9" />
+        <Text>
+          Please try adjusting your filters or visit our main page to browse our
+          full range of products.
+        </Text>
+        <Button size="md" component={Link} to="/shop">
+          Shop page
+        </Button>
+      </Stack>
+    </Card>
   );
 }
